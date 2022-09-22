754e601fd3c29e26fcaf4ce6760b195552250b23
Svetlana Nesterova <sveta59n@yahoo.com>
2022-09-22 18:53:27 -0400

    answering questions

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template.js');
+// const fs = require('fs');
 
-const profileDataArgs = process.argv.slice(2);
+// const generatePage = require('./src/page-template.js');
 
-const [nameUser, github]  = profileDataArgs;
+// const pageHTML = generatePage(nameUser, github);
 
-
-
-
-
-
-fs.writeFile('index.html', generatePage(nameUser, github), err => {
-  if (err) throw new Error(err);
+// fs.writeFile('index.html', pageHTML, err => {
+//   if (err) throw err;
   
-  console.log('Portfolio Complete! Check out index.html to see the output');
-})
+//   console.log('Portfolio Complete! Check out index.html to see the output');
+// });
+
+inquirer
+  .prompt ([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
