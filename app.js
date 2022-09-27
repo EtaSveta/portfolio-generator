ce39b7e88aea7b05fe9f1d7b45dddb3d46cf031b
Svetlana Nesterova <sveta59n@yahoo.com>
2022-09-27 13:09:41 -0400

    something

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-const fs = require('fs');
+const generateSite = require('.utils/generate-site.js')
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template.js');
 
@@ -183,26 +183,25 @@ const promptProject = portfolioData => {
 };
 
 promptUser()
-.then(promptProject)
-.then(portfolioData => {
-  const pageHTML = generatePage(portfolioData);
-
-  fs.writeFile('./dist/index.html', pageHTML, err => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    console.log('Portfolio Complete! Check out index.html to see the output');
-
-    fs.copyFile('./src/style.css', './dist/style.css', err => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log("Style Sheet copied sucessfully")
-    });
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
-});
+
+  
   
 
   
